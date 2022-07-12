b77f07470dda9bb87ff8c678a7dc75a2448d5531
akbarhlubis <alubis87@gmail.com>
2022-07-12 21:50:29 +0700

    first commit

---
+++ asset/js/script.js
@@ -0,0 +1,12 @@
+const kode_voucher = "1111111111"; // Try edit me
+const tri = "*111*" + kode_voucher + "#";
+const telkomsel = "*133*" + kode_voucher + "#";
+const xl = "*123*" + kode_voucher + "#";
+
+
+
+// Update header text
+document.querySelector("#hasil_input").innerHTML = tri;
+
+// Log to console
+console.log(xl);
