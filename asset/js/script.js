7ddbd69d85afcc899ab000eb45f7e179e1f9a2ce
akbarhlubis <alubis87@gmail.com>
2022-07-13 19:34:16 +0700

    update

--- asset/js/script.js
+++ asset/js/script.js
@@ -21,3 +21,18 @@ function InputOutput() {
     y = document.getElementById("output").innerHTML;
     document.querySelector("#output").innerHTML = x;
 }
+
+function myFunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("output");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  } 
