8cb9f954f4b33b9c2adf957803ce87e0828f3d6d
akbarhlubis <alubis87@gmail.com>
2022-07-13 12:33:31 +0700

    update

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,12 +1,23 @@
-const kode_voucher = "1111111111"; // Try edit me
-const tri = "*111*" + kode_voucher + "#";
-const telkomsel = "*133*" + kode_voucher + "#";
-const xl = "*123*" + kode_voucher + "#";
+// const kode_voucher = "1111111111"; // Try edit me
+// const tri = "*111*" + kode_voucher + "#";
+// const telkomsel = "*133*" + kode_voucher + "#";
+// const xl = "*123*" + kode_voucher + "#";
 
 
 
-// Update header text
-document.querySelector("#hasil_input").innerHTML = tri;
+// // // Update header text
+// document.getElementById("#output").innerHTML = x;
+// // document.querySelector("#output").innerHTML = xl;
+// // document.write(xl);
 
-// Log to console
-console.log(xl);
+// // Log to console
+// console.log(xl);
+
+function InputOutput() {
+    var x;
+    var y;
+
+    x = document.getElementById("input").value;
+    y = document.getElementById("output").innerHTML;
+    document.querySelector("#output").innerHTML = x;
+}
