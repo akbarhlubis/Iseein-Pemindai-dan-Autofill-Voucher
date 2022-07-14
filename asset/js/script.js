5c3836a892f0305fca954765693c189df9dd1793
akbarhlubis <alubis87@gmail.com>
2022-07-14 12:08:34 +0700

    update js html dan css

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,23 +1,29 @@
-// const kode_voucher = "1111111111"; // Try edit me
-// const tri = "*111*" + kode_voucher + "#";
-// const telkomsel = "*133*" + kode_voucher + "#";
-// const xl = "*123*" + kode_voucher + "#";
-
-
-
-// // // Update header text
-// document.getElementById("#output").innerHTML = x;
-// // document.querySelector("#output").innerHTML = xl;
-// // document.write(xl);
-
-// // Log to console
-// console.log(xl);
-
-function InputOutput() {
-    var x;
-    var y;
-
-    x = document.getElementById("input").value;
-    y = document.getElementById("output").innerHTML;
-    document.querySelector("#output").innerHTML = x;
-}
+function onlyNumberKey(evt) {
+    // Only ASCII character in that range allowed
+    let ASCIICode = evt.which ? evt.which : evt.keyCode;
+    return !(ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57));
+  }
+  
+  function inputOutput() {
+    let x;
+    let tri;
+    x = document.getElementById("inputKode").value;
+    tri = "*111*" + x + "#";
+    document.getElementById("outputKode").value = tri;
+  }
+  
+  function myFunction() {
+    /* Get the text field */
+    let copyText = document.getElementById("outputKode");
+  
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  
+    /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText.value);
+  
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
+  }
+  
